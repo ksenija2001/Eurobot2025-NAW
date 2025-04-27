2b3762876c97c62a019f3757c2945db2590d5210
mkukobat17 <120792992+mkukobat17@users.noreply.github.com>
2025-04-27 17:17:52 +0200

    Decommit

--- SIMA_V1/main/wifi/credentials/credentials.c
+++
@@ -1,9 +0,0 @@
-#include "credentials.h"
-
-const char* WIFI_SSID = "Aleksa";
-const char* WIFI_PASS = "10101010";
-
-const char* SERVER_IP = "10.242.113.144";
-const int SERVER_PORT = 9999;
-
-const char* SIMA_ID = "1";
